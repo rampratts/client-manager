1cb0b9ac461a9c332e3030c4e77d4ae95b82b303
Ramiro Meneses <xanco78@gmail.com>
2019-01-04 23:17:33 -0300

    Project starter files and navbar component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './App.css';
+
+import AppNavbar from "./components/layout/AppNavbar";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <h1>hello</h1>
+          </div>
+        </div>
+      </Router>
+
+    );
+  }
+}
+
+export default App;
